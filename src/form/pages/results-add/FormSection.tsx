b68fa831b9e9c1a4c7fdffd159862deed129f116
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-12-01 22:04:34 +0000

    Added in results form structure. Need to add verification and more details for the database

--- src/form/pages/results-add/FormSection.tsx
+++ src/form/pages/results-add/FormSection.tsx
@@ -2,11 +2,12 @@
 import SiteFormContext from "@/form/context/SiteFormContext"
 import { Checkbox } from "@/components/ui/checkbox"
 import { useContext } from "react"
-import type { CalibrationType } from "@/types/calibration"
 import type { AffinionCardType } from "@/types/affinion"
-import type { TestRangeType } from "@/types/calibration"
-import type { Hba1cRangeType } from "@/types/calibration"
-import type { FetchState } from "@/hooks/useFetchData"
+import { FormProvider, useForm } from "react-hook-form"
+import Hba1cSection from "./Hba1cSection"
+import LipidSection from "./LipidSection"
+import FormContext from "@/form/context/FormContext"
+
 
 export default function FormSection () {
 
@@ -16,8 +17,7 @@ export default function FormSection () {
         throw new Error('SiteFormContext has to be used within <SiteFormContext.Provider>')
     }
 
-    // Change type back to number
-    const { affinions } = siteFormContext
+    const { affinions } = siteFormContext    // Change type back to number
 
     return (
         <div className="flex flex-row w-full justify-around">
@@ -42,149 +42,45 @@ function AffinionResultCard ({ affinion }: AffinionResultCardProps) {
     if (siteFormContext === null) throw new Error('SiteFormContext has to be used within <SiteFormContext.Provider>')
     const { controls } = siteFormContext
 
+    const context = useContext(FormContext)
+    if (context === null) throw new Error('Error fetching the site')
+    const { site } = context;
+    if (site === null) throw new Error('Component rendered without site being selected')
+
+    const methods = useForm();
+    const { handleSubmit, setValue } = methods;
+
+    setValue("affinion_id", affinion.affinion_id)
+    setValue("site_id", site.site_id)
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+    })
+
     return (
-        <form className="bg-white p-4 rounded outline">
-            <div>
+        <FormProvider {...methods}>
+            <form className="bg-white p-4 rounded outline" onSubmit={onSubmit}>
                 <div>
-                    <p className="text-center">{affinion.name} | {affinion.nh_number}</p>
-                    <div className=" bg-blue-200 px-2 py-0.5 flex gap-2 justify-center items-center rounded">
-                        <Checkbox 
-                            className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
-                        />
-                        <label className="text-blue-500 text-sm">Not in rotation this week</label>
+                    <div>
+                        <p className="text-center">{affinion.name} | {affinion.nh_number}</p>
+                        <div className=" bg-blue-200 px-2 py-0.5 flex gap-2 justify-center items-center rounded">
+                            <Checkbox 
+                                className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
+                            />
+                            <label className="text-blue-500 text-sm">Not in rotation this week</label>
+                        </div>
+                        <Hba1cSection controlsData={controls} />
+                        <LipidSection controlsData={controls} />
+                        <div className=" bg-green-200 px-2 py-0.5 flex gap-2 justify-center items-center rounded">
+                            <Checkbox 
+                                className="data-[state=checked]:border-green-600 data-[state=checked]:bg-green-600 data-[state=checked]:text-white dark:data-[state=checked]:border-green-700 dark:data-[state=checked]:bg-green-700"
+                            />
+                            <label className="text-green-500 text-sm">Affinion Cleaned</label>
+                        </div>
                     </div>
-                    <Hba1cSection controlsData={controls}/>
-                    <LipidSection controlsData={controls}/>
-                    <div className=" bg-green-200 px-2 py-0.5 flex gap-2 justify-center items-center rounded">
-                        <Checkbox 
-                            className="data-[state=checked]:border-green-600 data-[state=checked]:bg-green-600 data-[state=checked]:text-white dark:data-[state=checked]:border-green-700 dark:data-[state=checked]:bg-green-700"
-                        />
-                        <label className="text-green-500 text-sm">Affinion Cleaned</label>
-                    </div>
                 </div>
-            </div>
-            <button type="submit">Submit Results</button>
-        </form>
-    )
-}
-
-function Loading () {
-    return (
-        <div>
-            <p>Loading...</p>
-        </div>
-    )
-}
-
-interface Hba1cSectionProps {
-    title: string,
-    testType: string,
-    controlsData: FetchState<CalibrationType>
-}
-
-function Hba1cSection ({ controlsData }: Hba1cSectionProps) {
-
-    const control = controlsData.data.find((item: CalibrationType) => { return item.test_type === 'hba1c' })
-
-
-    if (controlsData.loading) {
-        return (<Loading />)
-    } else if (control === undefined) {
-        return (
-            <p>Error msg</p>
-        )
-    } else {
-        return (
-            <div>
-                <p className="text-center font-bold text-gray-600">HBA1c</p>
-                <Hba1cInputRanges ranges={control.calibration_ranges}/>
-            </div>
-        )
-    }
-
-}
-
-interface LipidSectionProps {
-    testType: string,
-    controlsData: FetchState<CalibrationType>
-}
-
-function LipidSection ({ controlsData }: LipidSectionProps) {
-
-    const control = controlsData.data.find((item: CalibrationType) => { return item.test_type === 'lipids' })
-
-    if (controlsData.loading) {
-        return (<Loading />)
-    } else if (control == undefined) {
-        return (
-            <p>Error msg</p>
-        )
-    } else {
-        return (
-            <div>
-                <p className="text-center font-bold text-gray-600">Lipids</p>
-                <p>Total</p>
-                <LipidsInputRanges ranges={control.calibration_ranges.total} />
-                <p>HDL Cholesterol</p>
-                <LipidsInputRanges ranges={control.calibration_ranges.hdl} />
-                <p>Trigylcerides</p>
-                <LipidsInputRanges ranges={control.calibration_ranges.triglycerides} />
-            </div>
-        )
-    }
-}
-
-// New files
-
-
-interface Hba1cInputRangesProps {
-    ranges: Hba1cRangeType,
-}
-
-function Hba1cInputRanges ({ ranges }: Hba1cInputRangesProps) {
-
-    return (
-    <div className="flex flex-row gap-2">
-        <div className="flex flex-col">
-            <div className="flex flex-row justify-between">
-                <label className="font-bold">C1</label>
-                <p className="text-gray-500">{ranges.c1.low} - {ranges.c1.high}</p>
-            </div>
-            <input type="text" className="outline rounded"/>
-        </div>
-        <div className="flex flex-col">
-            <div className="flex flex-row justify-between">
-                <label className="font-bold">C2</label>
-                <p className="text-gray-500">{ranges.c2.low} - {ranges.c2.high}</p>
-            </div>
-            <input type="text" className="outline rounded"/>
-        </div>
-    </div>
-    )
-}
-
-interface LipidsInputRangesProps {
-    ranges: TestRangeType,
-}
-
-function LipidsInputRanges ({ ranges }: LipidsInputRangesProps) {
-
-    return (
-        <div className="flex flex-row gap-2">
-            <div className="flex flex-col">
-                <div className="flex flex-row justify-between">
-                    <label className="font-bold">C1</label>
-                    <p className="text-gray-500">{ranges.c1.low} - {ranges.c1.high}</p>
-                </div>
-                <input type="text" className="outline rounded"/>
-            </div>
-            <div className="flex flex-col">
-                <div className="flex flex-row justify-between">
-                    <label className="font-bold">C2</label>
-                    <p className="text-gray-500">{ranges.c2.low} - {ranges.c2.high}</p>
-                </div>
-                <input type="text" className="outline rounded"/>
-            </div>
-        </div>
+                <button type="submit" >Submit Results</button>
+            </form>
+        </FormProvider>
     )
 }
---
+++ src/form/pages/results-add/Hba1cSection.tsx
@@ -0,0 +1,66 @@
+import type { FetchState } from "@/hooks/useFetchData"
+import type { CalibrationType } from "@/types/calibration"
+import { useFormContext } from "react-hook-form"
+import type { Hba1cRangeType } from "@/types/calibration"
+
+interface Hba1cSectionProps {
+    controlsData: FetchState<CalibrationType>
+}
+
+export default function Hba1cSection ({ controlsData }: Hba1cSectionProps) {
+
+    const control = controlsData.data.find((item: CalibrationType) => { return item.test_type === 'hba1c' })
+
+    if (controlsData.loading) {
+        return (<Loading />)
+    } else if (control === undefined) {
+        return (
+            <p>Error msg</p>
+        )
+    } else {
+        return (
+            <div>
+                <p className="text-center font-bold text-gray-600">HBA1c</p>
+                <Hba1cInputRanges ranges={control.calibration_ranges}/>
+            </div>
+        )
+    }
+
+}
+
+interface Hba1cInputRangesProps {
+    ranges: Hba1cRangeType,
+}
+
+function Hba1cInputRanges ({ ranges }: Hba1cInputRangesProps) {
+
+    const { register } = useFormContext()
+
+    return (
+    <div className="flex flex-row gap-2">
+        <div className="flex flex-col">
+            <div className="flex flex-row justify-between">
+                <label className="font-bold">C1</label>
+                <p className="text-gray-500">{ranges.c1.low} - {ranges.c1.high}</p>
+            </div>
+            <input type="text" className="outline rounded" {...register("hba1c_c1", { required: "pass a result"})}/>
+        </div>
+        <div className="flex flex-col">
+            <div className="flex flex-row justify-between">
+                <label className="font-bold">C2</label>
+                <p className="text-gray-500">{ranges.c2.low} - {ranges.c2.high}</p>
+            </div>
+            <input type="text" className="outline rounded" {...register("hba1c_c2", { required: "pass a result"})}/>
+        </div>
+    </div>
+    )
+}
+
+function Loading () {
+    return (
+        <div>
+            <p>Loading...</p>
+        </div>
+    )
+}
+
