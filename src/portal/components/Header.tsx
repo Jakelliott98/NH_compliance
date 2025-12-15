a5b497e2822a6b17e0e21a84104b0a3a05d0911c
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2025-12-15 14:51:30 +0000

    Adding styling

--- src/portal/components/Header.tsx
+++ src/portal/components/Header.tsx
@@ -5,14 +5,21 @@ import { NavLink } from 'react-router'
 export default function Header () {
     
     return (
-    <div className='bg-white p-2 w-full flex justify-between content-center'>
-        <p>Logo</p>
+    <div className='bg-white px-5 py-4 w-full flex justify-between content-center'>
+        <p className='text-sm uppercase text-neutral-600'>Logo</p>
         <nav className='w-4/12 flex justify-between cursor-pointer'>
-            <NavLink to="Dashboard" className={({isActive}) => {return isActive ? 'text-blue-500' : 'tet-black'}}>Dashboard</NavLink>
-            <NavLink to="Compliance" className={( {isActive}) => {return isActive ? 'text-blue-500' : 'text-black'}}>Compliance</NavLink>
-            <NavLink to="Settings" className={( {isActive}) => {return isActive ? 'text-blue-500' : 'text-black'}}>Settings</NavLink>
+            <NavLink to="Dashboard" className={({isActive}) => {return isActive ? 'text-black' : 'text-neutral-400'}}>
+                <p className='text-sm uppercase'>Dashboard</p>
+            </NavLink>
+            <NavLink to="Compliance" className={( {isActive}) => {return isActive ? 'text-black' : 'text-neutral-400'}}>
+                <p className='text-sm uppercase'>Compliance</p>
+            </NavLink>
+            <NavLink to="Settings" className={( {isActive}) => {return isActive ? 'text-black' : 'text-neutral-400'}}>
+                <p className='text-sm uppercase'>Settings</p>
+
+            </NavLink>
         </nav>
-        <FontAwesomeIcon icon={faCircleUser} className='self-center'/>
+        <FontAwesomeIcon icon={faCircleUser} className='self-center text-lg text-neutral-600'/>
     </div>
     )
 }
--- src/portal/pages/compliance/CompliancePage.tsx
+++ src/portal/pages/compliance/CompliancePage.tsx
@@ -7,10 +7,10 @@ export default function CompliancePage () {
         <div>
             <div className="w-full flex justify-center">
                 <div className="w-4/12 flex justify-around rounded-xl py-1 px-2 bg-white">
-                    <NavLink to="Sites" className={({isActive}) => {return isActive ? 'text-blue-500' : 'text-black'}}>
+                    <NavLink to="Sites" className={({isActive}) => {return isActive ? 'text-black' : 'text-neutral-400'}}>
                         <p className="py-0.5 px-2 rounded-lg">Sites</p>
                     </NavLink>
-                    <NavLink to="Reports" className={({isActive}) => {return isActive ? 'text-blue-500' : 'text-black'}}>
+                    <NavLink to="Reports" className={({isActive}) => {return isActive ? 'text-black' : 'text-neutral-400'}}>
                         <p className="py-0.5 px-2 rounded-lg">Reports</p>
                     </NavLink>                    
                 </div>
