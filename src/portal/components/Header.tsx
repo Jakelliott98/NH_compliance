3aa01d64228b0adf8bdc44bc902e9fdcb9f4395c
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2026-01-08 10:54:21 +0000

    Added in small tweak for UI. Dashboard was index however, not displaying active nav

--- src/portal/components/Header.tsx
+++ src/portal/components/Header.tsx
@@ -1,14 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser, faGear, faClipboardList, faShieldHalved } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from 'react-router'
+import { NavLink, useLocation } from 'react-router'
 
 export default function Header () {
     
+    const location = useLocation().pathname
+    const isDashboard = location === "/Portal/Dashboard" || location === '/Portal';
+
     return (
     <div className='bg-white px-5 py-4 w-full flex justify-between items-center'>
         <p className='text-sm uppercase text-neutral-400'>CT</p>
         <nav className='w-4/12 flex justify-between cursor-pointer'>
-            <NavLink to="Dashboard" className={({isActive}) => {return isActive ? 'text-black' : 'text-neutral-400 hover:text-black'}}>
+            <NavLink to="Dashboard" className={isDashboard ? 'text-black' : 'text-neutral-400 hover:text-black'}>
                 <div className='flex gap-1 items-center'>
                     <FontAwesomeIcon icon={faClipboardList} className='text-sm'/>
                     <p className='text-sm uppercase'>Dashboard</p>
