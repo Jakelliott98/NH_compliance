bc0c09e2f1830995cf6d1b53a51015a47c4ea43f
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2026-01-09 12:01:02 +0000

    Linked slider to the rendering UI time periods for cleaning and calibration durations

--- src/portal/pages/settings/ComplianceRules.tsx
+++ src/portal/pages/settings/ComplianceRules.tsx
@@ -1,6 +1,10 @@
 import { Slider } from "@/components/ui/slider"
+import { useState } from "react"
 
 export default function ComplianceRules () {
+
+    const [cleaningValue, setCleaningValue] = useState([4])
+    const [calibrationValue, setCalibrationValue] = useState([7])
 
     return (
         <div className="py-4">
@@ -15,20 +19,25 @@ export default function ComplianceRules () {
                 </div>
                 <div className="flex-3 flex justify-around pt-4">
                     <div className="flex flex-col justify-center gap-4">
-                        <h2 className="font-medium ">Compliance for Cleaning Duration: <span className="font-bold">30 Days</span></h2>
+                        <h2 className="font-medium ">Affinion machines need to be calibrated every: <span className="font-bold">{cleaningValue[0]} Weeks</span></h2>
                         <Slider 
-                            defaultValue={[30]}
-                            max={60}
+                            defaultValue={[4]}
+                            max={12}
                             step={1}
+                            value={cleaningValue}
+                            onValueChange={value => setCleaningValue(value)}
                         />
                         <p className="text-sm text-gray-500 italic">Adjust the period for cleaning calibrations.</p>
                     </div>
                     <div className="flex flex-col justify-center gap-4">
-                        <h2 className="font-medium ">Compliance for Affinion Calibration Duration: <span className="font-bold">7 Days</span></h2>
+                        <h2 className="font-medium ">Affinions machines need to be cleaned every: <span className="font-bold">{calibrationValue[0]} Days</span></h2>
                         <Slider
-                            defaultValue={[7]}
-                            max={28}
+                            className="cursor-pointer"
+                            defaultValue={[1]}
+                            max={30}
                             step={7}
+                            value={calibrationValue}
+                            onValueChange={(value) => {return setCalibrationValue(value)}}
                         />
                         <p className="text-sm text-gray-500 italic">Adjust the period for how often the affinions should be calibrated</p>
                     </div>
