d6ae2562372636880a9ed02680e0ab613dc63fa0
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2026-02-13 23:06:24 +0000

    Added in consistent stlying to the navbar

--- src/portal/components/Header.tsx
+++ src/portal/components/Header.tsx
@@ -12,20 +12,20 @@ export default function Header () {
     return (
     <div className='bg-white px-5 py-4 w-full flex justify-between items-center'>
         <p className='sm-hidden-block text-sm uppercase text-neutral-400'>CT</p>
-        <nav className='hidden md:flex w-4/12 justify-between cursor-pointer'>
-            <NavLink to="Dashboard" className={isDashboard ? 'text-black' : 'text-neutral-400 hover:text-black'}>
+        <nav className='hidden md:flex cursor-pointer bg-bg rounded-lg'>
+            <NavLink to="Dashboard" className={`cursor-pointer py-2 px-4 rounded-lg ${isDashboard ? 'bg-primary text-primary-foreground' : 'text-neutral-light hover:text-neutral'}`}>
                 <div className='flex gap-1 items-center'>
                     <FontAwesomeIcon icon={faClipboardList} className='text-sm'/>
                     <p className='text-sm uppercase'>Dashboard</p>
                 </div>
             </NavLink>
-            <NavLink to="Compliance" className={( {isActive}) => {return isActive ? 'text-black' : 'text-neutral-400 hover:text-black'}}>
+            <NavLink to="Compliance" className={( {isActive}) => {return `cursor-pointer py-2 px-4 rounded-lg ${isActive ? 'bg-primary text-primary-foreground' : 'text-neutral-light hover:text-neutral'}`}}>
                 <div className='flex gap-1 items-center'>
                     <FontAwesomeIcon icon={faShieldHalved} className='text-sm'/>
                     <p className='text-sm uppercase'>Compliance</p>
                 </div>
             </NavLink>
-            <NavLink to="Settings" className={( {isActive}) => {return isActive ? 'text-black' : 'text-neutral-400 hover:text-black'}}>
+            <NavLink to="Settings" className={( {isActive}) => {return `cursor-pointer py-2 px-4 rounded-lg ${isActive ? 'bg-primary text-primary-foreground' : 'text-neutral-light hover:text-neutral'}`}}>
                 <div className='flex gap-1 items-center'>
                 <FontAwesomeIcon icon={faGear} className='text-sm'/>
                 <p className='text-sm uppercase'>Settings</p>
